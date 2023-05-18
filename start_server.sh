411ffd3710d38e317ac9ae0f42bceb6bdf99073a
Tanmay Trivedi <106506571+tanmay-trivedi-inferenz@users.noreply.github.com>
2023-05-18 17:59:00 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup gunicorn testDjango.wsgi:application --bind 0.0.0.0:8000 &
+gunicorn testDjango.wsgi:application --bind 0.0.0.0:8000 --daemon
 echo "Executed: nohup gunicorn testDjango.wsgi:application --bind 0.0.0.0:8000 --daemon &"
